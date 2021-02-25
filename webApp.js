f4db3098811fb54549d76906a85ba2b74f01c076
Donutask <Donutask@users.noreply.github.com>
2021-02-25 19:26:17 +1100

    update service worker path

--- webApp.js
+++ webApp.js
@@ -1,6 +1,6 @@
 function Register() {
     if (!navigator.serviceWorker.controller) {
-        navigator.serviceWorker.register("/sw.js").then(function(reg) {
+        navigator.serviceWorker.register("sw.js").then(function(reg) {
             console.log("Service worker has been registered for scope: " + reg.scope);
         });
     }
